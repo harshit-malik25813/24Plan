079245eb9ab1d1a1b0e696642b61dfb71f9ce559
harshit-malik25813 <harshitfr25@gmail.com>
2026-08-14 15:46:57 +0530

    chore: add initial files

---
+++ src/dbconnect.py
@@ -0,0 +1,8 @@
+# Connection pipeline to interface the information stored in the database
+
+# Function to fetch information
+def fetch(sub, extra):
+    return "TODO"
+# Function to feed information
+def feed(sub, extra):
+    return "TODO"
---
+++ src/main.py
@@ -0,0 +1,6 @@
+# 24 Plan
+# Developed and open-sourced by harshit-malik25813
+# All the code is licensed under MIT License
+# GitHub repo: https://github.com/harshit-malik25813/24Plan
+
+from dbconnect import fetch
